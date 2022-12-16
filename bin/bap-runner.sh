58407e9df374a0c420a60dd8af833f8efccf5342
SpudGunMan <kellykeeton@yahoo.com>
2022-12-16 10:29:19 -0800

    RC3.5b

--- bin/bap-runner.sh
+++ bin/bap-runner.sh
@@ -2,7 +2,7 @@
 # ------------------------------------------------------------------
 # process manager
 # ------------------------------------------------------------------
-SH_VERSION=1.0.0
+SH_VERSION=1.0.1
 #Error and DEBUG
 if [ ${DEBUG:=0} -eq 1 ];then echo -e "DEBUG: bap-runner.sh"; fi
 if test -f ".dev"; then set -Eeoxu;trap 'echo >&2 "Error - exited with status $? at line $LINENO:"; 
